3fb2c7cd3c2941bd307e904f69ffd59c95aaea48
Euronym <brunos-martins@hotmail.com>
2022-05-24 17:28:51 -0300

    adicao do notebook

--- main.py
+++ main.py
@@ -3,11 +3,17 @@
 # Author : Bruno Santos Martins
 
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D  
 import math
 
 G = 10E9
 
-building_dimensions = (200, 250, 300) 
+building_height = [0, 200]
+
+building_width = [0, 50]
+
+xx, yy = np.meshgrid(np.arange(50), np.arange(50))
 
 carrier_frequency = 2 * G
 
@@ -22,23 +28,28 @@ n_users_floor = 20
 height_floors = 5
 
 max_iterations = 500
-
-n_floors = round(building_dimensions[2] / height_floors)
-
-coordinates_users = np.ones((n_floors, 20, 3)) 
-
-theta_users = np.ones((n_floors, 20))
-
+# total number of floors 
+#n_floors = round(building_dimensions[2] / height_floors)
+# coordinates of users in the building
+#coordinates_users = np.ones((n_floors, 20, 3)) 
+# angle between uav and users 
+#theta_users = np.ones((n_floors, 20))
+# initial coordinates of the uav
 coordinates_uav = [0, 0, 0]
 
 distance_user_building, distance_drone_user, theta = 0, 0, 0  
 
+fig = plt.figure()
+
+ax = fig.add_subplot(111, projection='3d')
+
+ax.plot3D(50, 50, 0)
+
+#m_1 = 
+
+#m_2 = 
+
 '''
-
-m_1 = 
-
-m_2 = 
-
 l_1, l_2, l_3 = 0, 0, 0
 
 for i in range(n_users_floor * n_floors):
